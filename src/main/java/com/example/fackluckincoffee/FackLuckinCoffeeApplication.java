ea8fa8dfbb5413b6c97c12f0ce381e16a0a8f386
Guuugao <583357007@qq.com>
2024-07-12 08:28:38 +0800

    初始化提交

---
+++ src/main/java/com/example/fackluckincoffee/FackLuckinCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.fackluckincoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FackLuckinCoffeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FackLuckinCoffeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fackluckincoffee/FackLuckinCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fackluckincoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FackLuckinCoffeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
